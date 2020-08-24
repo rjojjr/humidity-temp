c550f9b172bc810b23cf26c2b1c3d9bc9d8999a2
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-24 15:55:21 -0500

    add-sql - Change sql date to datetime - RK

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE py_temp;
-CREATE TABLE readings(id INT NOT NULL AUTO_INCREMENT,room VARCHAR(50),temp INT NOT NULL,room VARCHAR(50) humidity INT NOT NULL,time DATE,PRIMARY KEY ( id ));
+CREATE TABLE readings(id INT NOT NULL AUTO_INCREMENT,room VARCHAR(50),temp INT NOT NULL,room VARCHAR(50) humidity INT NOT NULL,time DATETIME,PRIMARY KEY ( id ));
 CREATE USER 'pythonuser'@'localhost' IDENTIFIED BY 'UEAkJFcwcmRQQCQkVzByZAo=';
 GRANT ALL PRIVILEGES ON py_temp TO 'pythonuser'@'localhost';
