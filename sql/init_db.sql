88774dc6c6a1699d86451a1e99d95a9db53dd194
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-23 20:13:30 -0500

    add-sql - Add MySql file - RK

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE py_temp;
-CREATE TABLE readings(id INT NOT NULL AUTO_INCREMENT,temp INT NOT NULL,humidity INT NOT NULL,submission_date DATE,PRIMARY KEY ( id ));
+CREATE TABLE readings(id INT NOT NULL AUTO_INCREMENT,temp INT NOT NULL,humidity INT NOT NULL,time DATE,PRIMARY KEY ( id ));
 CREATE USER 'pythonuser'@'localhost' IDENTIFIED BY 'UEAkJFcwcmRQQCQkVzByZAo=';
 GRANT ALL PRIVILEGES ON py_temp TO 'pythonuser'@'localhost';
