dd23bd6fd3f5afc34ce513f1473ee54d4cac6cac
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-25 12:58:33 -0500

    init-rest-api - Add read endpoint - RK

--- src/api/controller.py
+++ src/api/controller.py
@@ -1,10 +1,16 @@
 import flask
-
+from dht.read import Read
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
     return flask.jsonify({"msg": "pi-temp API"})
+
+@app.route('/read', methods=['GET'])
+def read():
+    reader = Read()
+    readings = reader.dummy()
+    return flask.jsonify({"temp": readings[0], "humidity": readings[1]})
 
 app.run()
---
+++ src/api/dht/read.py
@@ -0,0 +1,15 @@
+import dht.Adafruit_DHT
+
+class Read:
+    def getTemp(self):
+        sensor = Adafruit_DHT.DHT22
+        pin = 4
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT_PIN)
+        temp = (1.8 * temperature) + 32
+        humidity = humidity
+        t = str((temp))
+        h = str((humidity))
+        return [t, h]
+
+    def dummy(self):
+        return ["70", "60"]
