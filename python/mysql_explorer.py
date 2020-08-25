8adedffe47d468ceb78b664c3e4f5b723b23cf96
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-25 10:46:58 -0500

    mysql-explorer - Add print all rows - RK

--- python/mysql_explorer.py
+++ python/mysql_explorer.py
@@ -5,11 +5,14 @@ userInput = -1
 while userInput != 0:
     print("1. Avg Temp")
     print("2. Avg Humidity")
+    print("3. Print all Rows")
     print("0. Exit")
     userInput = input()
     if userInput == 1:
         print("Avg. Temp = " + str(sql.avgTemp()))
     elif userInput == 2:
         print("Avg. Humidity = " + str(sql.avgHumidity()))
-    else:
+    elif userInput == 3:
+        sql.getAllReadings()
+    elif userInput != 0:
         print("Invalid input")
