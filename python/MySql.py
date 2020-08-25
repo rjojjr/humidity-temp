5e3d76208d488185b15e6b4a5c5154be600f7f75
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-25 09:58:58 -0500

    mysql-explorer - Get avg temp - RK

--- python/MySql.py
+++ python/MySql.py
@@ -6,7 +6,7 @@ SQL_DEBUG = 1
 
 class MySql:
 
-    def execute(self, statement):
+    def executeStatement(self, statement):
         try:
             mydb = mysql.connector.connect(
                 host="192.168.1.12",
@@ -23,11 +23,22 @@ class MySql:
             if (SQL_DEBUG == 1):
                 print("Committing row to db")
             mydb.commit()
+            records = mycursor.fetchall()
+            return records
         except:
             print("An SQL error has happened")
+            return ""
 
     def insertRecord(self, temp, humidity):
         now = datetime.datetime.now()
         formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
         statement = "INSERT INTO readings (`room`, `temp`, `humidity`, `time`) VALUES ('office','" + temp + "','" + humidity + "','"  + formatted_date + "');"
-        self.execute(statement)
+        self.executeStatement(statement)
+
+    def avgTemp(self):
+        statement = "SELECT AVG(temp) 'Average Temp' FROM readings;"
+        result = executeStatement(statement)
+        print(result)
+
+    def getAllReadings(self):
+        statement = "SELECT * FROM readings;"
