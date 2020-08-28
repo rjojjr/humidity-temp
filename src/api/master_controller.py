ed4f4657e5c7f741e409be29a15c39ff9a076cc4
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-28 14:27:49 -0500

    story-1 - Add master api port 8080 - RK

--- src/api/master_controller.py
+++ src/api/master_controller.py
@@ -52,7 +52,7 @@ def server():
         summary = SummaryService()
         return flask.jsonify(summary.getSummaries())
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 def main():
     server()
