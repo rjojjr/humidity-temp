5a6b64577c36ba3958e7bc9ae1d00e4ac1fb3cf7
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-08-28 12:52:58 -0500

    story-1 - Add esp boot.py - RK

---
+++ src/esp/boot.py
@@ -0,0 +1,26 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import esp
+
+#esp.osdebug(None)
+
+#import webrepl
+
+#webrepl.start()
+
+
+def connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('<ssid>', '<key>')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+def no_debug():
+    import esp
+    # this can be run from the REPL as well
+    esp.osdebug(None)
