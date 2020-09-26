33ee3756297e0a74f61cb63c39d23bab161eebec
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-09-26 13:35:47 -0500

    story-21 - Add Interval model - RK

---
+++ src/api/models/interval.py
@@ -0,0 +1,6 @@
+class Interval:
+    def __init__(self, office, bedroom, freezer, outside):
+        self.office = office
+        self.bedroom = bedroom
+        self.freezer = freezer
+        self.outside = outside
