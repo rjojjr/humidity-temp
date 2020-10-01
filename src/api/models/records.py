68dcaeca4cc3fbaa170034e530c35b605e8a1a19
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-10-01 16:30:51 -0500

    story-55 - Add reading record model - RK

---
+++ src/api/models/records.py
@@ -0,0 +1,6 @@
+class ReadingRecord:
+    def __init__(self, room, temp, humidity, date):
+        self.room = room
+        self.temp = temp
+        self.humidity = humidity
+        self.date = date
