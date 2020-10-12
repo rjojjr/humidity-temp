1acb767985bcded221efeaeab9b1df193917bb23
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-10-12 11:12:54 -0500

    story-55 - Add room sum model - RK

--- src/api/models/records.py
+++ src/api/models/records.py
@@ -4,3 +4,8 @@ class ReadingRecord:
         self.temp = temp
         self.humidity = humidity
         self.date = date
+
+class RoomSum:
+    def __init__(self, room, sum):
+        self.room = room
+        self.sum = sum
