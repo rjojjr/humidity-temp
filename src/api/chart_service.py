4657a37ce3e37e1a59fdc48003a8d6c18c1d5a68
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-10-12 11:47:35 -0500

    story-55 - Add get day temp average to chart service - RK

--- src/api/chart_service.py
+++ src/api/chart_service.py
@@ -6,6 +6,8 @@ from api.models.summary import Summary
 from api.models.interval_request import IntervalRequest
 from api.models.interval import Interval
 from api.models.interval import Intervals
+from api.models.records import ReadingRecord
+from api.models.records import RoomSum
 
 class ChartService:
 
@@ -26,3 +28,84 @@ class ChartService:
             30,
             31
         ]
+
+    def getSum(self, roomSums, room):
+        for sum in roomSums:
+            if sum.room == room:
+                return sum.sum
+        return None
+
+    def getDate(self, reading):
+        return reading.date
+
+    def addRoomReading(self, roomSums, reading, type):
+        for sum in roomSums:
+            if sum.room == reading.room:
+                if type == "temp":
+                    sum.sum = sum.sum + reading.temp
+                else:
+                    sum.sum = sum.sum + reading.humidity
+        return roomSums
+
+    def splitDate(self, date):
+        split = []
+        days = date.split(" ")[0]
+        hours = date.split(" ")[1]
+        for unit in days:
+            split.append(int(unit))
+        for unit in hours:
+            split.append(int(unit))
+        return split
+
+    def compareDateSplit(self, subject, start, end):
+        split = self.splitDate(subject)
+        if split[0] >= start[0] and split[0] <= end[0]:
+            if split[1] >= start[1] and split[1] <= end[1]:
+                if split[2] >= start[2] and split[2] <= end[2]:
+                    if split[3] >= start[3] and split[3] <= end[3]:
+                        if split[4] >= start[4] and split[4] <= end[4]:
+                            if split[5] >= start[5] and split[5] <= end[5]:
+                                return True
+                            else:
+                                return False
+                        else:
+                            return False
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    def getDayAvgApi(self, endRange, intervals, year, month, day, sdt, edt):
+        startTime = (datetime.datetime(year, month, day, 0, 0, 0, 0) + datetime.timedelta(hours = 0)).strftime('%Y-%m-%d %H:%M:%S')
+        endTime = (datetime.datetime(year, month, day, 0, 0, 0, 0) + datetime.timedelta(hours = 24)).strftime('%Y-%m-%d %H:%M:%S')
+        records = self.sql.getRecordsBetween(startTime, endTime)
+        records.sort(key=self.getDate)
+        cursor = 0
+        for k in range(0, endRange):
+            avgDate = (datetime.datetime(year, month, day, 0, 0, 0, 0) + datetime.timedelta(hours = k)).strftime('%Y-%m-%d %H:%M:%S')
+            if day == 0:
+                sDate = (datetime.datetime(year, month, day, 0, 0, 0, 0) + datetime.timedelta(hours = (k - 1))).strftime('%Y-%m-%d %H:%M:%S')
+            else:
+                sDate = (datetime.datetime(year, month, day, 0, 0, 0, 0) + datetime.timedelta(hours = (k - 1))).strftime('%Y-%m-%d %H:%M:%S')
+            eDate = (datetime.datetime(year, month, day, 0, 0, 0, 0) + datetime.timedelta(hours = (k + 1))).strftime('%Y-%m-%d %H:%M:%S')
+            intervals.append(self.getInterval(records, cursor, self.splitDate(sDate), self.splitDate(eDate), avgDate, "temp"))
+
+    def getInterval(self, readings, cursor, startDate, endDate, intervalDate, type):
+        roomSums = []
+        for room in self.sql.getRooms():
+            roomSums.append(RoomSum(room, 0))
+        first = True
+        for i in range(cursor, len(readings)):
+            cursor = i
+            if self.compareDateSplit(self.splitDate(readings[i].date), startDate, endDate):
+                if first:
+                    first = False
+                self.addRoomReading(roomSums, readings[i], type)
+            else:
+                if first == False:
+                    break
+        return Interval(intervalDate, self.getSum(roomSums, "office"), self.getSum(roomSums, "bedroom"), self.getSum(roomSums, "freezer"), self.getSum(roomSums, "outside"))
