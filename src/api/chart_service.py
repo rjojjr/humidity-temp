6ea02c72fc055ac30d91264ee7469dff8f4df552
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-10-12 10:42:05 -0500

    story-55 - Add chart service - RK

---
+++ src/api/chart_service.py
@@ -0,0 +1,28 @@
+from api.dht.read import Read
+from api.dht.MySql import MySql
+import datetime
+from api.models.summary import Summary
+
+from api.models.interval_request import IntervalRequest
+from api.models.interval import Interval
+from api.models.interval import Intervals
+
+class ChartService:
+
+    def __init__(self):
+        self.sql = MySql()
+        self.read = Read()
+        self.daysInMonth = [
+            31,
+            28,
+            31,
+            30,
+            31,
+            30,
+            31,
+            31,
+            30,
+            31,
+            30,
+            31
+        ]
