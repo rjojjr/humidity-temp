cd8ba1d5f3dfca11995375d493bfaea23e50d0de
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-10-15 16:41:55 -0500

    story-50 - Add transfer record sql method - RK

--- src/api/dht/MySql.py
+++ src/api/dht/MySql.py
@@ -104,6 +104,18 @@ class MySql:
         statement = "INSERT INTO readings (`room`, `temp`, `humidity`, `time`) VALUES ('" + room + "','" + temp + "','" + humidity + "','"  + formatted_date + "');"
         self.executeStatement(statement)
 
+    def insertRecord(self, temp, humidity, room, time_stamp):
+        statement = "INSERT INTO readings (`room`, `temp`, `humidity`, `time`) VALUES ('" + room + "','" + temp + "','" + humidity + "','"  + time_stamp + "');"
+        self.executeStatement(statement)
+
+    def transferRecords(self):
+        statement = "SELECT temp, humidity, time, room, id FROM readings;"
+        result = self.executeStatementReturn(statement, "192.168.1.12")
+        records = []
+        for i in result:
+            print("transferring record " + id + " from old host")
+            self.insertRecord(i[0], i[1], i[3], i[2])
+
     def avgTemp(self, room):
         statement = "SELECT AVG(temp) 'Average Temp' FROM readings WHERE room = '" + room + "';"
         result = self.executeStatementReturn(statement)
