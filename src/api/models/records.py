4d0ef17b41cb6dd74af24da67c86e143f8dae1b6
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-10-15 19:04:28 -0500

    story-50 - DEBUG: resume record transfer - RK

--- src/api/models/records.py
+++ src/api/models/records.py
@@ -5,6 +5,13 @@ class ReadingRecord:
         self.humidity = humidity
         self.date = date
 
+    def __init__(self, room, temp, humidity, date, time):
+        self.room = room
+        self.temp = temp
+        self.humidity = humidity
+        self.date = date
+        self.time = time
+
 class RoomSum:
     def __init__(self, room, sum, count):
         self.room = room
